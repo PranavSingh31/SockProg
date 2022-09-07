0674950cb6e003c4a8d91a947176c6a3b86c4e41
Pranav <prsingh1347@gmail.com>
2022-09-07 23:17:04 +0530

    .

--- TCP Chat Server/TCP_Client.cpp
+++ TCP Chat Server/TCP_Client.cpp
@@ -6,23 +6,24 @@
 #define MAX 80
 #define PORT 8080
 #define SA struct sockaddr
-
 void func(int sockfd){
 	char buff[MAX];
 	int n;
-	for (;;) {
+	for (;;){
 		bzero(buff, sizeof(buff));
-		
+
 		printf("Enter the string : ");
-		n=0;
+		n = 0;
 		while ((buff[n++] = getchar()) != '\n');
-		
+
 		write(sockfd, buff, sizeof(buff));
+
 		bzero(buff, sizeof(buff));
-		
+
 		read(sockfd, buff, sizeof(buff));
+
 		printf("From Server : %s", buff);
-		if((strncmp(buff,"exit",4))==0){
+		if ((strncmp(buff, "exit", 4)) == 0){
 			printf("Client Exit...\n");
 			break;
 		}
@@ -34,26 +35,23 @@ int main(){
 	struct sockaddr_in servaddr, cli;
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd == -1){
+	if (sockfd == -1){
 		printf("socket creation failed...\n");
 		exit(0);
 	}
 	else printf("Socket successfully created..\n");
-		
-		
+
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	servaddr.sin_port = htons(PORT);
 
-	if(connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0){
+	if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) != 0){
 		printf("connection with the server failed...\n");
 		exit(0);
 	}
 	else printf("connected to the server..\n");
 
 	func(sockfd);
-
 	close(sockfd);
 }
-
